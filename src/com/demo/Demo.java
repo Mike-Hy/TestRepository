3f3cc87231d9ba91d0fbbfe76f8fa0f9f34dfbf4
michael.ng <michael8@qq.vip.com>
2017-05-08 16:11:09 +0800

    首次提交

---
+++ src/com/demo/Demo.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * @author xiao
+ */
+public class Demo {
+	
+	public static void main(String[] args) {
+		ArrayList<String> arrayList = new ArrayList<String>();
+		HashMap<Object, Object> hashMap = new HashMap<>();
+		System.out.println("it's demo");
+	}
+
+}
